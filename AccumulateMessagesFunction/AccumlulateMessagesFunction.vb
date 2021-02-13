bef1ccfc5e9a0c3215596c24e20dda80f34d4f3f
Victoria Gomez <77286079+ToriGomez@users.noreply.github.com>
2021-02-13 09:32:42 -0700

    Add project files.

---
+++ AccumulateMessagesFunction/AccumlulateMessagesFunction.vb
@@ -0,0 +1,44 @@
+﻿'Tori Gomez
+'RCET0265
+'Spring 2021
+'Accumulate Messages Function
+
+
+Option Explicit On
+Option Strict On
+
+Module AccumlulateMessagesFunction
+
+    Sub Main()
+        Dim userInput As String
+
+        Console.WriteLine("Name:")
+        userInput = Console.ReadLine()
+        UserInputCheck(userInput)
+
+        Console.WriteLine("Age:")
+        userInput = Console.ReadLine()
+        UserInputCheck(userInput)
+
+        MsgBox(UserMessages("", False))
+        Console.Read()
+
+    End Sub
+
+    Function UserInputCheck(ByVal userInput As String) As String
+        If userInput = "" Then
+            Return UserMessages(($"{userInput}"), True)
+        Else
+            Return UserMessages(($"{userInput}"), False)
+        End If
+    End Function
+    Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
+        Static messages As String
+        If Not clear Then
+            messages &= newMessage & vbNewLine
+        Else
+            messages = ""
+        End If
+        Return messages
+    End Function
+End Module
